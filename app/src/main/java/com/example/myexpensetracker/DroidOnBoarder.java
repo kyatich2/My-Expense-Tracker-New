7d1c8855e112fe6b580fb15ea553c1c4aa6d94a4
kyatich2 <kangwony.yatich@strathmore.edu>
2019-11-01 14:47:08 +0300

    Changed Mpesa implementation

---
+++ app/src/main/java/com/example/myexpensetracker/DroidOnBoarder.java
@@ -0,0 +1,14 @@
+package com.example.myexpensetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DroidOnBoarder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_droid_on_boarder);
+    }
+}
